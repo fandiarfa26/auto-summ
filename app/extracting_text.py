2040f74cc2cd2724089347234a8a23b5dbf2cfeb
Fandi Arfa <arfabuma.987@gmail.com>
2020-02-05 11:36:37 +0700

    fixing process extracting text

--- app/extracting_text.py
+++ app/extracting_text.py
@@ -1,12 +1,18 @@
 import PyPDF2
 
 def extracting_text(file):
-    
+
+    # create a pdf reader
     pdfReader = PyPDF2.PdfFileReader(file)
+
+    # get total pdf page number
+    totalPageNumber = pdfReader.numPages
     fulltext = ''
 
-    for p in range(0, pdfReader.numPages):
+    for p in range(0, totalPageNumber):
         pageObj = pdfReader.getPage(p)
         fulltext += pageObj.extractText()
+    
+    #print(fulltext)
 
-    return fulltext
+    return fulltext.replace('\n', '').replace('  ',' ')
