4b7b7277a8b11d0cccafc453a5867ba2e8309988
M. Fandi Arfabuma <arfabuma.987@gmail.com>
2020-02-06 21:32:41 +0700

    try to setting db

---
+++ app/models.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from app import db
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    data_file = db.Column(db.LargeBinary)
+    summaries = db.relationship('Summary', backref='book', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Book ID:{}>'.format(self.id)
+
+class Summary(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text())
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'))
+
+    def __repr__(self):
+        return '<Summary {}>'.format(self.id)
