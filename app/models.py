214a25905a22bb8661c7ff0a1530599308e9c2a2
Fandi Arfa <arfabuma.987@gmail.com>
2020-02-21 08:21:13 +0700

    add Dataset and change models db

--- app/models.py
+++ app/models.py
@@ -4,19 +4,29 @@ from app import db
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
-    pages = db.Column(db.Integer())
-    filename = db.Column(db.String(100))
-    summaries = db.relationship('Summary', backref='book', lazy='dynamic')
+    cover = db.Column(db.String(100))
+    chapters = db.relationship('Chapter', backref='book', lazy='dynamic')
 
     def __repr__(self):
         return '<Book ID:{}>'.format(self.id)
+
+class Chapter(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    pages = db.Column(db.Integer())
+    filename = db.Column(db.String(100))
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'))
+    summaries = db.relationship('Summary', backref='chapter', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Chapter ID:{}>'.format(self.id)
 
 class Summary(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.Text())
     keywords = db.Column(db.String(255), default="-")
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    book_id = db.Column(db.Integer, db.ForeignKey('book.id'))
+    chapter_id = db.Column(db.Integer, db.ForeignKey('chapter.id'))
 
     def __repr__(self):
         return '<Summary {}>'.format(self.id)
